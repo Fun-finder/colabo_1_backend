8d90e9095b1bfcd6da449b79a0483b4e23326795
goonle <74893925+goonle@users.noreply.github.com>
2024-03-24 20:55:16 +0900

    Create spring project
    스프링 프로젝트를 생서합니다.
    dependency
    - java21
    - gradle
    - jpa
    - spring-web
    - lombok
    -postgresql driver
    -rest api doc
    
    DB는 써본 postgreSQL을 사용하고 DB 데이터 처리를 JPA와 빌더패턴을 쉽게 이용하기 위해 lombok을 추가했습니다.
    
    테스트 코드작성을 위해 h2 db도 사용합니다.
    
    API 통신을 규격화된 restAPI를 사용하려고 합니다.

---
+++ colab1/src/main/java/com/colab1/funfinder/Colab1Application.java
@@ -0,0 +1,13 @@
+package com.colab1.funfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Colab1Application.class, args);
+	}
+
+}
---
+++ colab1/src/test/java/com/colab1/funfinder/Colab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colab1.funfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Colab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

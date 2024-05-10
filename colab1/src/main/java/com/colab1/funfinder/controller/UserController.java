7ac0d1a56cfe5ef520a667d75b384fb2d96093ab
namdream <skaekqls789@naver.com>
2024-05-10 23:11:38 +0900

    로그인 cors 오류 수정

--- colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
+++ colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
@@ -15,58 +15,39 @@ import com.colab1.funfinder.dto.LoginRequest;
 import com.colab1.funfinder.entity.User;
 import com.colab1.funfinder.service.UserService;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/user")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = {"*"})
 public class UserController {
 
-		private final UserService userService;
-		/**
-		 * 로그인 요청 
-		 * @param request
-		 * @return 로그인 아이디 (long)
-		 * @throws Exception
-		 */
-		@PostMapping("/login")
-		public ResponseEntity login(@RequestBody LoginRequest request, HttpServletRequest httpServletRequest) throws Exception{
-			Optional<User> optionalUser = Optional.ofNullable(userService.login(request));
-			System.out.println("=====================");
-			System.out.println("login id : "+request.getLoginId());
-			System.out.println("login pw : "+request.getPassword());
-			System.out.println("optionalUser : "+optionalUser);
-			System.out.println("=====================");
-			if(optionalUser.isEmpty()) return new ResponseEntity(HttpStatus.BAD_REQUEST);
-			
-			User loginUser = optionalUser.get();
-			// 세션을 생성하기 전에 기존의 세션 파기
-			httpServletRequest.getSession().invalidate();
-			HttpSession session = httpServletRequest.getSession(true);
-			
-	        // 세션에 userId를 넣어줌
-	        session.setAttribute("userId", loginUser.getLoginId());
-	        session.setMaxInactiveInterval(1800); // Session이 30분동안 유지
-	        System.out.println("session : "+ session.getAttribute("userId"));
-	        //로그인 화면에 뿌려주고 싶은게 있으면
-	        //화면에 뿌리는 DTO를 만들어서 값을 넣어주자
-			return new ResponseEntity(HttpStatus.OK);
-		}
-		/**
-		 * 회원가입
-		 * DB insert, session.setAttribute
-		 * 화면에 로그인 중인 것을 알리면 페이지 이동(vue 에서는 어떻게 할 것인가)
-		 * @param request
-		 * @return ReponseEntity
-		 * @throws Exception
-		 */
-		@PostMapping("/join")
-		@ResponseStatus(HttpStatus.OK)
-		public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request) throws Exception{
-			return ResponseEntity.ok(null);
-		}
-		
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest request, HttpServletRequest httpServletRequest) throws Exception {
+        Optional<User> optionalUser = Optional.ofNullable(userService.login(request));
+        if(optionalUser.isEmpty()) return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
+        User loginUser = optionalUser.get();
+        httpServletRequest.getSession().invalidate();
+        HttpSession session = httpServletRequest.getSession(true);
+        session.setAttribute("userId", loginUser.getId());
+        session.setMaxInactiveInterval(1800);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/join")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request) throws Exception {
+        return ResponseEntity.ok(null);
+    }
+
+
 }

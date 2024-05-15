c1198298e2717417fad3364de3fd9871d9822af4
nam <skaekqls789@naver.com>
2024-05-15 21:28:05 +0900

    login 기능 수정

--- colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
+++ colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
@@ -6,35 +6,42 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.colab1.funfinder.dto.JoinRequest;
 import com.colab1.funfinder.dto.LoginRequest;
 import com.colab1.funfinder.dto.LoginResponse;
 import com.colab1.funfinder.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/v1/user/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request, HttpServletRequest httpServletRequest) {
         try {
-            // String token = userService.authenticate(request.getLoginId(), request.getPassword());
-            // if (token != null) {
-            //     httpServletRequest.getSession(true).setAttribute("token", token);
-            //     httpServletRequest.getSession().setMaxInactiveInterval(1800);
-            //     return ResponseEntity.ok(new LoginResponse("Login successful", token));
-            // } else {
-            //     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid loginId or password");
-            // }
-            return ResponseEntity.status(200).body("response ok!!!");
+            System.out.println("request.getLoginId(): " + request.getLoginId() + ", request.getPassword(): " + request.getPassword());
+            String token = userService.authenticate(request.getLoginId(), request.getPassword());
+            if (token != null) {
+                httpServletRequest.getSession(true).setAttribute("token", token);
+                httpServletRequest.getSession().setMaxInactiveInterval(1800);
+                return ResponseEntity.ok(new LoginResponse("Login successful", token));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid loginId or password");
+            }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
         }

7af32d15d0af3b29260924d8d85876d51e2bd218
nam <skaekqls789@naver.com>
2024-05-15 18:52:10 +0900

    404에러 임시 처리

--- colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
+++ colab1/src/main/java/com/colab1/funfinder/controller/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.colab1.funfinder.dto.LoginRequest;
 import com.colab1.funfinder.dto.LoginResponse;
 import com.colab1.funfinder.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -21,17 +23,18 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
+    @PostMapping("/api/v1/user/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request, HttpServletRequest httpServletRequest) {
         try {
-            String token = userService.authenticate(request.getLoginId(), request.getPassword());
-            if (token != null) {
-                httpServletRequest.getSession(true).setAttribute("token", token);
-                httpServletRequest.getSession().setMaxInactiveInterval(1800);
-                return ResponseEntity.ok(new LoginResponse("Login successful", token));
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid loginId or password");
-            }
+            // String token = userService.authenticate(request.getLoginId(), request.getPassword());
+            // if (token != null) {
+            //     httpServletRequest.getSession(true).setAttribute("token", token);
+            //     httpServletRequest.getSession().setMaxInactiveInterval(1800);
+            //     return ResponseEntity.ok(new LoginResponse("Login successful", token));
+            // } else {
+            //     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid loginId or password");
+            // }
+            return ResponseEntity.status(200).body("response ok!!!");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
         }
